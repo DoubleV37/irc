fbcc07df37a5f00a639823d1786c918902b3eef5
ltuffery <123221865+ltuffery@users.noreply.github.com>
2023-10-21 10:53:18 +0200

    Add message command execution

---
+++ srcs/commands/Message.cpp
@@ -0,0 +1,28 @@
+#include "Message.hpp"
+#include <string>
+
+Message::Message() : ACommand("message", "/message <user> <message>")
+{
+}
+
+Message::~Message()
+{
+}
+
+bool Message::execute(std::vector<std::string> args, User *user, Channel *channel, Server *server)
+{
+	std::string message = "";
+
+	(void)channel;
+	(void)server;
+	if (args.size() < 2)
+	{
+		return false;
+	}
+	for (size_t i = 0; i < args.size(); i++)
+	{
+		message.append(args.at(i));
+	}
+	user->send(message);
+	return true;
+}
