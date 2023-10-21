4b854c0ce61f2aa8dc9e032665a99165cfb7c1d3
Valentin Viovi <vviovi@1G4.42angouleme.fr>
2023-10-21 12:39:06 +0200

    add function for creation of socket server and start implementation of the server class

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -3,80 +3,108 @@
 /*                                                        :::      ::::::::   */
 /*   Server.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gazzopar <gazzopar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vviovi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 11:21:34 by gazzopar          #+#    #+#             */
-/*   Updated: 2023/10/20 13:37:15 by gazzopar         ###   ########.fr       */
+/*   Updated: 2023/10/21 12:34:12 by vviovi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
 Server::Server() {
-    
+
+}
+
+Server::Server(int port) {
+	this->_port = port;
+	create_socket_server();
+	for (int i = 0 ; i < MAX_CONNECTIONS ; i++)
+		this->all_connections[i] = -1;
+	this->nb_connections = 1;
+	this->all_connections[0] = this->_socket;
 }
 
 Server::Server( const Server& obj ) {
 
-    *this = obj;
+	*this = obj;
 }
 
 Server& Server::operator=( const Server& obj ) {
-    
-    if (this != &obj )
-        return *this;
-    return *this;    
+
+	if (this != &obj )
+		return *this;
+	return *this;
 }
 
 Server::~Server() {
-    
+
+}
+
+int Server::create_socket_server()
+{
+	struct sockaddr_in addr;
+
+	this->_socket = socket(AF_INET, SOCK_STREAM, 0); // ici on recupère le FD du socket
+	if (this->_socket == -1)
+		return (0);//erreur socket
+	int opt = 1; // Verifier pourquoi 1
+	setsockopt(this->_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+	addr.sin_family = AF_INET; // IPV4
+	addr.sin_addr.s_addr = INADDR_ANY; // 0.0.0.0
+	addr.sin_port = htons(this->_port); // Port
+	if (bind(this->_socket, (struct sockaddr *)&addr, sizeof(addr)) != 0)
+		return (0);
+	if (listen(this->_socket, MAX_CONNECTIONS))
+		return (0);
+	return (1);
 }
 
 void Server::addUser( User* user ) {
 
-    (void)user;
+	(void)user;
 }
 
 void Server::addChannel( Channel* channel ) {
 
-    (void)channel;   
+	(void)channel;
 }
 
 std::vector<User*> Server::getUsers() {
 
-    return this->_users;
+	return this->_users;
 }
 
 std::map<std::string, Channel*> Server::getChannels() {
 
-    return this->_channels;
+	return this->_channels;
 }
 
 User* Server::getUserByUsername( std::string const & userName ) const {
 
-    (void)userName;
-    return NULL;     
+	(void)userName;
+	return NULL;
 }
 
 Channel* Server::getChannelByName( std::string const & channel ) const {
 
-    (void)channel;    
-    return NULL;   
+	(void)channel;
+	return NULL;
 }
 
 void Server::deleteUser(User* user) {
 
-    (void)user;
+	(void)user;
 }
 
 ACommand* Server::getCommand( std::string const & command ) const  {
 
-    (void)command;
-    return NULL;
+	(void)command;
+	return NULL;
 }
 
 void Server::addCommand( std::string const & name, ACommand* command ) {
-    
-    (void)name;
-    (void)command;
+
+	(void)name;
+	(void)command;
 }
--- srcs/Server.hpp
+++ srcs/Server.hpp
@@ -3,53 +3,66 @@
 /*                                                        :::      ::::::::   */
 /*   Server.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gazzopar <gazzopar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vviovi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 11:21:37 by gazzopar          #+#    #+#             */
-/*   Updated: 2023/10/20 13:35:23 by gazzopar         ###   ########.fr       */
+/*   Updated: 2023/10/21 12:07:15 by vviovi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-#include <string>
+#include <iostream>
 #include <vector>
 #include "User.hpp"
 #include "Channel.hpp"
 #include "ACommand.hpp"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+#define BUFFER 5000
+#define MAX_CONNECTIONS 10
 
 class ACommand;
 
 class Server {
-    
-    private:
-        int _port;
-        std::string _password;
-        std::vector<User*> _users;
-        std::map<std::string, Channel*> _channels;
-        std::map<std::string, ACommand*> _command;
-        
-        void addUser( User* user );
-        std::vector<User*> getUsers();
-        std::map<std::string, Channel*> getChannels();
-        void deleteUser(User*);
-        void deleteChannel(Channel*);
-        ACommand* getCommand( std::string const & command ) const;
-        void addCommand( std::string const & name, ACommand* command );
-        bool isCommand( std::string const & name );
-        void run();
-        // SWITCH CASE POUR DISPATCH
-        void dispatch( std::string const & buffer);
 
-    public:
-        Server();
-        Server( std::string ServerName );
-        // Ajouter constructeur paramètres
-        Server( const Server& obj );
-        Server& operator=( const Server& obj );
-        ~Server(); 
+	private:
+		int	_port;
+		int	_socket;
+		int	all_connections[MAX_CONNECTIONS];
+		int	nb_connections;
 
-        User* getUserByUsername( std::string const & userName ) const;
-        Channel* getChannelByName( std::string const & channel ) const;    
-        void addChannel( Channel* channel );
+
+
+		std::string	_password;
+		std::vector<User*>	_users;
+		std::map<std::string, Channel*>		_channels;
+		std::map<std::string, ACommand*>	_command;
+
+		void addUser( User* user );
+		std::vector<User*> getUsers();
+		std::map<std::string, Channel*> getChannels();
+		void deleteUser(User*);
+		void deleteChannel(Channel*);
+		ACommand* getCommand( std::string const & command ) const;
+		void addCommand( std::string const & name, ACommand* command );
+		bool isCommand( std::string const & name );
+		void run();
+		// SWITCH CASE POUR DISPATCH
+		void dispatch( std::string const & buffer);
+		int create_socket_server();
+
+	public:
+		Server();
+		Server(int port);
+		// Ajouter constructeur paramètres
+		Server( const Server& obj );
+		Server& operator=( const Server& obj );
+		~Server();
+
+		User* getUserByUsername( std::string const & userName ) const;
+		Channel* getChannelByName( std::string const & channel ) const;
+		void addChannel( Channel* channel );
 
 };
