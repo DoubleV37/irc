e9014427e5f76dc4f684eacc763dd3f33319f7f0
Valentin Viovi <vviovi@1F2.42angouleme.fr>
2023-11-02 13:42:19 +0100

    rework message pour prendre en compte les espaces dans les messages et l'envoi de messages dans les channels

--- srcs/commands/Message.hpp
+++ srcs/commands/Message.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Message.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gazzopar <gazzopar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vviovi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 10:59:15 by ltuffery          #+#    #+#             */
-/*   Updated: 2023/10/31 17:03:19 by gazzopar         ###   ########.fr       */
+/*   Updated: 2023/11/02 12:58:25 by vviovi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ class Message : public ACommand
 	public:
 		Message();
 		~Message();
-    	bool execute( std::vector<std::string> args, User* user, Channel* channel, Server* server );
+    	bool	execute( std::vector<std::string> args, User* user, Channel* channel, Server* server );
+		void	sendBroadcastMessage(Server *server, Channel *channel, std::string message, User *origin_user);
 
 };
